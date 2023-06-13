c33166db17e8d0fa5e6b92e2d3207b94c58954f8
Matt Bakaitis <matt@bakaitis.com>
2023-06-13 15:26:38 -0400

    got rid of the awful table, used unordered list

--- components/composite/TopMenu.js
+++ components/composite/TopMenu.js
@@ -2,19 +2,15 @@ import { Bars3Icon } from '@heroicons/react/24/outline'
 
 export default function TopMenu({ className }) {
   return (
-    <div className="flex flex-col justify-end rtl:overflow-visible  group">
-      <table className="table-fixed rtl-fix">
-        <thead>
-          <tr>
-            <th><Bars3Icon className={className} /></th>
-          </tr>
-        </thead>
-        <tbody className="hidden border border-purple-300 group-hover:flex group-hover:flex-col group-hover:whitespace-nowrap group-hover:justify-end group-hover:px-4 bg-white absolute">
-          <tr ><td>Item 1</td></tr>
-          <tr ><td>Item 2</td></tr>
-          <tr ><td>Item 3</td></tr>
-        </tbody>
-      </table>
+    <div className="flex flex-col justify-end rtl:overflow-visible group">
+      <ul className="rtl-fix absolute">
+        <li><Bars3Icon className={className} /></li>
+        <div className="hidden border ltr-fix border-purple-300 group-hover:flex group-hover:flex-col group-hover:whitespace-nowrap group-hover:justify-end group-hover:px-4 bg-white absolute">
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </div>
+      </ul>
     </div>
   )
 }
